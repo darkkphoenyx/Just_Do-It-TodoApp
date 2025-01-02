f61786e3e1ce854dc6eed62ffe531d59b06413fa
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2025-01-02 18:08:52 +0545

    login form lined with backend api

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+// import HomePage from "../pages/Homepage/HomePage";
+import Login from "../pages/Login/Login";
+import HomePage from "../pages/Homepage/HomePage";
 
 export default function AppRoutes() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/" element={<Login onClose={() => {}} />}></Route>
+        <Route path="/homepage" element={<HomePage />}></Route>
       </Routes>
     </>
   );
