7ffdb70d67045156cf1a953fd2a2e140f4ae6bbe
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2025-01-02 17:02:24 +0545

    fixed the issues dealing with toggling the status of todo that do not exist

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,31 +1,32 @@
-import express, {
-    RequestHandler,
-    NextFunction,
-    Request,
-    Response,
-} from 'express'
-import cors from 'cors'
-import todosRouter from './routes/todo.router'
-import userRouter from './routes/auth.router'
-import buildError from './utils/build-error'
+import express, { RequestHandler, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import todosRouter from './routes/todo.router';
+import userRouter from './routes/auth.router';
+import buildError from './utils/build-error';
 import dotenv from 'dotenv';
 dotenv.config();
-const app = express()
 
-app.use(express.json())
-const port = process.env.PORT || 3000
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-app.use(cors())
-app.use('/todos', todosRouter)
-app.use('/user', userRouter)
+    console.log(`Server is running on port ${port}`);
+});
 
-//Error handler
+app.use(cors());
+app.use('/todos', todosRouter);
+app.use('/user', userRouter);
 
+// Dynamic Error Handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    const error = buildError(err)
-    res.status(error.code).json({ error })
-})
+    const error = buildError(err);
+    res.status(error.code).json({
+        error: {
+            message: error.message,
+            details: error.details || null,
+        },
+    });
+});
 
-export default app
+export default app;
